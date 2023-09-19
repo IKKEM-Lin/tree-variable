d359fd564dce8c5ece2a84c1afd595c9c25e8e4a
IKKEM-Lin <122335184+IKKEM-Lin@users.noreply.github.com>
2023-09-19 17:32:51 +0800

    5 files modified, 2 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,139 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { Switch, Collapse, Input, Button } from 'antd';
+import { PlusCircleFilled } from '@ant-design/icons';
+import type { CollapseProps } from 'antd';
+import { Editor } from './component/Editor';
+import './App.css';
+
+async function getData() {
+  const res = [
+    { fileName: 'file1.json', code: 'uerwexcnlasfe', enviroment: {} },
+    { fileName: 'file2.json', code: 'asdfwegfd123', enviroment: {} },
+    { fileName: 'file3.json', code: '1231241vasdfa', enviroment: {} },
+  ];
+  return res;
+}
+
+const cache: any = {};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [environmentCode, setEnvironmentCode] = useState('');
+  const [environmentValidate, setEnvironmentValidate] = useState(false);
+  const [advanceMode, setAdvanceMode] = useState(false);
+
+  const [items, setItems] = useState<CollapseProps['items']>([]);
+  const [itemsCode, setItemsCode] = useState<{ [key: string]: string }>({});
+
+  cache.itemsCode = itemsCode;
+  cache.items = items;
+
+  useEffect(() => {
+    getData().then((res) => {
+      const newItemsCode: any = {};
+      const data = res.map((item) => {
+        const key = `${Math.random()}`;
+        newItemsCode[key] = item.code;
+        return {
+          key: key,
+          label: item.fileName,
+          children: (
+            <>
+              <Input
+                defaultValue={item.fileName}
+                onChange={(evt) => {
+                  const val = evt.target.value;
+                  const currentItemIndex = cache.items.findIndex(
+                    (item) => item.key === key
+                  );
+                  // console.log({val, currentItemIndex})
+                  if (currentItemIndex === -1) {
+                    return;
+                  }
+                  cache.items[currentItemIndex].label = val;
+                  setItems([...cache.items]);
+                }}
+              />
+              <div style={{ height: '40vh', width: '80vw', marginTop: '5px' }}>
+                <Editor
+                  code={item.code}
+                  isDark={true}
+                  onChange={(code) =>
+                    setItemsCode({ ...cache.itemsCode, [key]: code })
+                  }
+                />
+              </div>
+            </>
+          ),
+        };
+      });
+      setItems(data);
+      setItemsCode(newItemsCode);
+    });
+  }, []);
+
+  const addItem = () => {
+    const key = `${Math.random()}`;
+    const defaultFileName = ""
+    const newItem = {
+      key: key,
+      label: defaultFileName,
+      children: (
+        <>
+          <Input
+            defaultValue={defaultFileName}
+            onChange={(evt) => {
+              const val = evt.target.value;
+              const currentItemIndex = cache.items.findIndex(
+                (item) => item.key === key
+              );
+              // console.log({val, currentItemIndex})
+              if (currentItemIndex === -1) {
+                return;
+              }
+              cache.items[currentItemIndex].label = val;
+              setItems([...cache.items]);
+            }}
+          />
+          <div style={{ height: '40vh', width: '80vw', marginTop: '5px' }}>
+            <Editor
+              code={""}
+              isDark={true}
+              onChange={(code) =>
+                setItemsCode({ ...cache.itemsCode, [key]: code })
+              }
+            />
+          </div>
+        </>
+      ),
+    };
+    setItems([...items, newItem])
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      Advance Mode:{'  '}
+      <Switch
+        checked={advanceMode}
+        onChange={(checked) => setAdvanceMode(checked)}
+      />
+      <div style={{ height: '20vh', width: '80vw' }}>
+        <div style={{ height: '100%' }} hidden={!advanceMode}>
+          <Editor
+            isDark={true}
+            language="json"
+            onChange={(newCode, validate) => {
+              setEnvironmentValidate(validate);
+              setEnvironmentCode(newCode);
+            }}
+          />
+        </div>
+        {!advanceMode && environmentCode}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Collapse accordion items={items} />
+      <Button type="primary" shape="circle" icon={<PlusCircleFilled />} onClick={() => addItem()} />
+      {Object.values(itemsCode).join(' ')}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/component/Editor.tsx
@@ -0,0 +1,133 @@
+import MonacoEditor, {
+  loader,
+  useMonaco,
+  DiffEditor,
+} from '@monaco-editor/react';
+import * as monaco from 'monaco-editor';
+import { editor, Uri } from 'monaco-editor';
+import { useEffect, useRef, useState } from 'react';
+
+loader.config({ monaco });
+
+import { JSONSchema7 } from 'json-schema';
+import classes from './Editor.module.css';
+
+type EditorProps = {
+  code: string;
+  schema?: JSONSchema7 | string;
+  onChange: (newCode: string, validated: boolean) => void;
+  diffView?: boolean;
+  isDark?: boolean;
+  language?: 'json';
+  // onClose: () => void;
+};
+
+const monacoEditorOptions: editor.IStandaloneEditorConstructionOptions = {
+  // minimap: {
+  //   enabled: false,
+  // },
+  automaticLayout: true,
+  scrollBeyondLastLine: false,
+};
+
+export function Editor({
+  code,
+  onChange,
+  schema,
+  diffView,
+  isDark,
+  language,
+}: EditorProps) {
+  const [editorVal, setEditorVal] = useState<string>(code);
+  const [markers, setMarkers] = useState<editor.IMarker[]>([]);
+  // const _schema = typeof schema === "string" ? parseStr2JSON(schema) : schema;
+  // const monacoEl = useMonaco();
+  const editorRef = useRef<editor.IStandaloneCodeEditor>();
+  const [mountDone, setMountDone] = useState(false);
+
+  useEffect(() => {
+    const editor = editorRef.current;
+    if (!editor) {
+      return;
+    }
+    editor.addCommand(monaco.KeyMod.Alt | monaco.KeyCode.KeyS, function () {
+      editor?.trigger('keyboard', 'editor.action.triggerSuggest', {});
+    });
+  }, [editorRef, mountDone]);
+
+  useEffect(() => {
+    // console.log({ editorRef }, editorRef.current?.getModel());
+    editorRef.current?.createDecorationsCollection([]);
+    onChange && onChange(editorVal, markers.length === 0);
+    // monacoEl?.Range;
+  }, [editorVal, markers]);
+
+  return (
+    <div className={classes.editorWrapper}>
+      <div
+        style={{
+          position: 'relative',
+          height: '100%',
+          boxSizing: 'border-box',
+        }}
+      >
+        <div
+          style={{ height: '100%' }}
+          className={(diffView && classes.hidden) || undefined}
+        >
+          <MonacoEditor
+            language={language}
+            // path={String(modelUri)}
+            value={editorVal}
+            theme={isDark ? 'vs-dark' : 'vs-light'}
+            onChange={(newCode) => {
+              setEditorVal(newCode || '');
+            }}
+            height={`100%`}
+            options={monacoEditorOptions}
+            onValidate={(marks) => {
+              setMarkers(marks);
+            }}
+            onMount={(editor) => {
+              editorRef.current = editor;
+              setMountDone(true);
+            }}
+          />
+        </div>
+        {diffView && (
+          <div
+            style={{ height: '100%' }}
+            className={(isDark && classes.darkBackgound) || undefined}
+          >
+            <DiffEditor
+              language={language}
+              original={code}
+              modified={editorVal}
+              theme={isDark ? 'vs-dark' : 'vs-light'}
+              height={`100%`}
+              options={{
+                ...monacoEditorOptions,
+                originalEditable: false,
+                renderSideBySide: false,
+                readOnly: true,
+              }}
+            />
+          </div>
+        )}
+      </div>
+      {/* <div
+          className={`${classes.validate} ${isDark && classes.validateDark}`}
+        >
+          {markers.map((marker) => {
+            const { message, startColumn, startLineNumber } = marker;
+            const errorRange = `[Ln ${startLineNumber}, Col ${startColumn}]`;
+            return (
+              <div key={errorRange + message}>
+                {message} {errorRange}
+              </div>
+            );
+          })}
+        </div> */}
+    </div>
+  );
+}
