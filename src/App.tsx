807d55f1e2780e9f586523c7d01c0f5d713726e1
IKKEM-Lin <122335184+IKKEM-Lin@users.noreply.github.com>
2023-09-20 11:37:15 +0800

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,59 @@
-import { useEffect, useState } from 'react';
-import { Switch, Collapse, Input, Button } from 'antd';
-import { PlusCircleFilled } from '@ant-design/icons';
+import { useEffect, useState, FC } from 'react';
+import {
+  Switch,
+  Collapse,
+  Input,
+  Button,
+  Space,
+  Tooltip,
+  Typography,
+  Popconfirm,
+} from 'antd';
+import { PlusCircleOutlined, DeleteOutlined } from '@ant-design/icons';
 import type { CollapseProps } from 'antd';
 import { Editor } from './component/Editor';
 import './App.css';
 
+const DeleteButton: FC<{ onConfirm: (evt: any) => void }> = ({ onConfirm }) => {
+  return (
+    <Popconfirm
+      title="Delete the file"
+      description="Are you sure to delete this file?"
+      onConfirm={onConfirm}
+      onCancel={(evt) => evt?.stopPropagation()}
+      okText="Yes"
+      cancelText="No"
+    >
+      <Button
+        // type="second"
+        danger
+        shape="circle"
+        icon={<DeleteOutlined color="#a00" />}
+        onClick={(evt) => {
+          evt?.stopPropagation();
+        }}
+      ></Button>
+    </Popconfirm>
+  );
+};
+
 async function getData() {
   const res = [
-    { fileName: 'file1.json', code: 'uerwexcnlasfe', enviroment: {} },
-    { fileName: 'file2.json', code: 'asdfwegfd123', enviroment: {} },
-    { fileName: 'file3.json', code: '1231241vasdfa', enviroment: {} },
+    {
+      fileName: 'file1.json',
+      code: 'uerwexcnlasfe',
+      enviroment: [{ name: 'v1', type: 'constant', value: '123' }],
+    },
+    {
+      fileName: 'file2.json',
+      code: 'asdfwegfd123',
+      enviroment: [{ name: 'v2', type: 'ref', value: 'https://github.com' }],
+    },
+    {
+      fileName: 'file3.json',
+      code: '1231241vasdfa',
+      enviroment: [{ name: 'v3', type: 'path', value: './local/temp.dump' }],
+    },
   ];
   return res;
 }
@@ -17,9 +61,10 @@ async function getData() {
 const cache: any = {};
 
 function App() {
-  const [environmentCode, setEnvironmentCode] = useState('');
+  const [environmentCode, setEnvironmentCode] = useState<string>('');
   const [environmentValidate, setEnvironmentValidate] = useState(false);
   const [advanceMode, setAdvanceMode] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const [items, setItems] = useState<CollapseProps['items']>([]);
   const [itemsCode, setItemsCode] = useState<{ [key: string]: string }>({});
@@ -27,15 +72,30 @@ function App() {
   cache.itemsCode = itemsCode;
   cache.items = items;
 
+  const genLabel = (label: string, key: string) => <Space>
+    {label}
+    <DeleteButton
+      onConfirm={(evt) => {
+        evt?.stopPropagation();
+        setItems(cache.items.filter((item) => item.key !== key));
+        const temp = { ...cache.itemsCode };
+        delete temp[key];
+        setItemsCode(temp);
+      }}
+    />
+  </Space>
+
   useEffect(() => {
+    setLoading(true);
     getData().then((res) => {
+      const newEnviroment = res.flatMap((item) => item.enviroment || []);
       const newItemsCode: any = {};
       const data = res.map((item) => {
         const key = `${Math.random()}`;
         newItemsCode[key] = item.code;
         return {
           key: key,
-          label: item.fileName,
+          label: genLabel(item.fileName, key),
           children: (
             <>
               <Input
@@ -49,7 +109,7 @@ function App() {
                   if (currentItemIndex === -1) {
                     return;
                   }
-                  cache.items[currentItemIndex].label = val;
+                  cache.items[currentItemIndex].label = genLabel(val, key);
                   setItems([...cache.items]);
                 }}
               />
@@ -68,19 +128,21 @@ function App() {
       });
       setItems(data);
       setItemsCode(newItemsCode);
+      setEnvironmentCode(JSON.stringify(newEnviroment, null, 2));
+      setLoading(false);
     });
   }, []);
 
   const addItem = () => {
     const key = `${Math.random()}`;
-    const defaultFileName = ""
+    const defaultFileName = '';
     const newItem = {
       key: key,
-      label: defaultFileName,
+      label: genLabel(defaultFileName, key),
       children: (
         <>
           <Input
-            defaultValue={defaultFileName}
+            defaultValue={''}
             onChange={(evt) => {
               const val = evt.target.value;
               const currentItemIndex = cache.items.findIndex(
@@ -90,13 +152,13 @@ function App() {
               if (currentItemIndex === -1) {
                 return;
               }
-              cache.items[currentItemIndex].label = val;
+              cache.items[currentItemIndex].label = genLabel(val, key);
               setItems([...cache.items]);
             }}
           />
           <div style={{ height: '40vh', width: '80vw', marginTop: '5px' }}>
             <Editor
-              code={""}
+              code={''}
               isDark={true}
               onChange={(code) =>
                 setItemsCode({ ...cache.itemsCode, [key]: code })
@@ -106,19 +168,29 @@ function App() {
         </>
       ),
     };
-    setItems([...items, newItem])
+    setItems([...items, newItem]);
+  };
+
+  if (loading) {
+    return;
   }
 
   return (
-    <>
-      Advance Mode:{'  '}
-      <Switch
-        checked={advanceMode}
-        onChange={(checked) => setAdvanceMode(checked)}
-      />
+    <Space direction="vertical">
+      <Typography.Title level={4} editable>{"Title"}</Typography.Title>
+        Description:
+      <Input.TextArea rows={5} />
+      <Space>
+        Advance Mode:
+        <Switch
+          checked={advanceMode}
+          onChange={(checked) => setAdvanceMode(checked)}
+        />
+      </Space>
       <div style={{ height: '20vh', width: '80vw' }}>
         <div style={{ height: '100%' }} hidden={!advanceMode}>
           <Editor
+            code={environmentCode}
             isDark={true}
             language="json"
             onChange={(newCode, validate) => {
@@ -130,9 +202,14 @@ function App() {
         {!advanceMode && environmentCode}
       </div>
       <Collapse accordion items={items} />
-      <Button type="primary" shape="circle" icon={<PlusCircleFilled />} onClick={() => addItem()} />
+      <Button
+        type="primary"
+        shape="circle"
+        icon={<PlusCircleOutlined />}
+        onClick={() => addItem()}
+      />
       {Object.values(itemsCode).join(' ')}
-    </>
+    </Space>
   );
 }
 
